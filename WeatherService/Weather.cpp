fda5c5c9d118c953dc58f908755a71e177aead6a
”Maxim <Darky@darky0x0.ru>
2023-05-30 09:33:19 +0300

    Had been added output

---
+++ WeatherService/Weather.cpp
@@ -0,0 +1,25 @@
+#include "Weather.h"
+
+Weather::Weather(string city, double lon, double lat, double temperature, string weather, double windSpeed, int clouds) {
+
+	this->city = city;
+	this->lon = lon;
+	this->lat = lat;
+	this->temperature = temperature;
+	this->weather = weather;
+	this->windSpeed = windSpeed;
+	this->clouds = clouds;
+
+}
+
+void Weather::getWeather(){
+
+	cout << city << ": lon(" << lon << "), lat(" << lat << "), t(" << temperature << "), погода(" << weather << "), скорость ветра(" << windSpeed << "), облачность(" << clouds << ")\n";
+
+}
+
+std::ostream& operator<<(std::ostream& out, Weather weatherrr)
+{
+	out << weatherrr.city << ": lon(" << weatherrr.lon << "), lat(" << weatherrr.lat << "), t(" << weatherrr.temperature << "), погода(" << weatherrr.weather << "), скорость ветра(" << weatherrr.windSpeed << "), облачность(" << weatherrr.clouds << ")\n";
+	return out;
+}
---
+++ WeatherService/WeatherService.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "Weather.h"
+#include <string>
+using namespace std;
+
+
+void main() {
+
+	setlocale(0, "");
+
+	Weather weather("Киров", 49.6601, 58.5966, 5.69, "дождик", 4.27, 100);
+
+	cout << weather;
+
+}
