faf31fbdf9f49e665baa270cdfb2cf0aaf232c45
”Maxim <Darky@darky0x0.ru>
2023-06-01 20:55:04 +0300

    test

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void main() {
 
 	setlocale(0, "");
-
+	
 	Weather weather("Киров", 49.6601, 58.5966, 5.69, "дождик", 4.27, 100);
 
 //	cout << weather;
@@ -17,9 +17,8 @@ void main() {
 
 	XmlService xs;
 
-	Weather w = xs.getWeather("weather.xml");
+	Weather w(xs.getWeather("weather.xml"));
 
 	cout << w;
-
 
 }
